936cbce260a07bd40179bb3b4e52131330b9821b
ResminiLoris <resminiloris@gmail.com>
2021-09-26 00:05:03 +0200

    print_average(no_function)

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    function print_numbers($min,$max){
+    /*function print_numbers($min,$max){
         for ($i = $min; $i <= $max; ++$i) {
             $numbers[] = $i;
         }
@@ -7,5 +7,32 @@
             echo '<li>' . $number . '</li>';
         }
     }
-    print_numbers(0,15);
-?>
+    print_numbers(0,15);*/
+
+    $students=[
+        [
+            "name"=>"loris",
+            "votes"=>["9","6","8",]
+        ],
+        [
+            "name"=>"lex",
+            "votes"=>["10","5","2",]
+        ],
+        [
+            "name"=>"leo",
+            "votes"=>["7","6","6",]
+        ],
+        [
+            "name"=>"simo",
+            "votes"=>["3","4","7",]
+        ],
+    ];
+    
+    foreach ($students as $student ){
+        echo $student["name"] . "<br>";
+        $average = 0;
+        foreach ($student["votes"] as $vote )
+        $average = $average + $vote;
+        echo 'MEDIA:' .  round($average / count($student["votes"])) . "<br>";
+    };
+    ?>
